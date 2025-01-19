32ff7ef3c6240d514fe09aa54d53353cf67f6fe3
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2025-01-19 15:15:07 -0600

    theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +13,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <>
+    <html lang="en" suppressHydrationWarning>
+    <head />
+    <body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+        >
         {children}
-      </body>
-    </html>
+      </ThemeProvider>
+    </body>
+  </html>
+        </>
   );
 }
